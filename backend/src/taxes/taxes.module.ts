6df7622c965012f5cf27d7f0032666ae02989d70
Yanomafpk <yanoma.konwski@sou.inteli.edu.br>
2025-11-12 11:18:16 -0300

    feat: adiciona crud taxas

--- backend/src/taxes/taxes.module.ts
+++ backend/src/taxes/taxes.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TaxesService } from './taxes.service';
 import { TaxesController } from './taxes.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [TaxesController],
   providers: [TaxesService],
 })
